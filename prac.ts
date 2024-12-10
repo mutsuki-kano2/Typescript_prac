8e65136a8e205a2ea9434465d65ec1d5b9626182
mutsuki-kano2 <kano@genz.jp>
2024-12-10 11:24:12 +0900

    Create prac.ts

---
+++ prac.ts
@@ -0,0 +1,16 @@
+function fizzBuzz(n: number): void {
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// 例えば、1から100までFizzBuzzを実行する
+fizzBuzz(100);
